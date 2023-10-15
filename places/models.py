69000694986bcefa8d49bcd2252a5efba179825e
Daniyar Akparov <daniyarkorp@gmail.com>
2023-10-15 21:16:12 +0300

    add places, model, views, urls

--- places/models.py
+++ places/models.py
@@ -1,3 +1,24 @@
+from tkinter import Place
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.IntegerField(default=0)
+    is_publicated = models.BooleanField(default=True)
+
+    # метод отображения объекта
+    def __str__(self):
+        return self.name
+
+    # настройки класса
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'Места'
+        ordering = ['name'] # сортировка по умолчанию по имени (можно по любому др. полю)
--- places/views.py
+++ places/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+# нам нужно получить весь список мест и отобразить
+def places(request):
+    place_objects = Place.objects.all() # отправляем запросы в базу данных SELECT *from
+    return render(request, 'places.html', {'places':place_objects})
